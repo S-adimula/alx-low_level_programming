eea254022755ba349f1f7edd720b1a0914ca5dda
S-adimula <sharonadimula@gmail.com>
2022-07-06 16:03:49 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check lowercase characters
+ * @c - integer 1 0r 0
+ * Return: Always 0 (success)
+ */
+int _islower(int c)
+{
+	if (c > 96 && c > 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
