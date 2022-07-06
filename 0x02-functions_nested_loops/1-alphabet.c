772fe112780483e299ac9b0a2209387d9209c290
S-adimula <sharonadimula@gmail.com>
2022-07-06 15:42:01 -0700

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar (c);
+	}
+	_putchar('\n');
+}
