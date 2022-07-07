3a5e5245f6104926ca4f473d5f406745d1d10a1a
S-adimula <sharonadimula@gmail.com>
2022-07-06 17:04:00 -0700

    task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#incude "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @c: integer
+ * Return: last digit
+ */
+int print_last_digit(int c)
+{
+	c = c % 10;
+
+	if (c < 10)
+	{
+		c = c * -1;
+	}
+
+	_putchar('0' + c);
+	return (c);
+}
